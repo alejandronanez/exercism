167144362b190baad1fd05df60bb2f52cf91fae3
Alejandro Nanez <git@alejandro.dev>
2025-04-27 18:34:10 -0500

    ruby - two fer

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -2,3 +2,15 @@
 Write your code for the 'Two Fer' exercise in this file. Make the tests in
 `two_fer_test.rb` pass.
 =end
+
+class TwoFer
+  def self.two_fer(name = nil)
+    if name
+      "One for #{name}, one for me."
+    else
+      "One for you, one for me."
+    end
+  end
+end
+
+TwoFer.two_fer("Alejo")
