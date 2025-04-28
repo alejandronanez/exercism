499fcc96ed5c33e8be1c0db1fc01f87f30427d5f
Alejandro Nanez <git@alejandro.dev>
2025-04-27 19:06:57 -0500

    ruby: assembly line

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,25 @@
 class AssemblyLine
+  BASE_ITEMS_PRODUCED_BY_HOUR = 221
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    if @speed >= 1 && @speed <= 4
+      BASE_ITEMS_PRODUCED_BY_HOUR * @speed
+    elsif @speed >= 5 && @speed <= 8
+      (BASE_ITEMS_PRODUCED_BY_HOUR * @speed) * 0.9
+    elsif @speed == 9
+      (BASE_ITEMS_PRODUCED_BY_HOUR * @speed) * 0.8
+    else
+      (BASE_ITEMS_PRODUCED_BY_HOUR * @speed) * 0.77
+    end
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    items_per_minute = production_rate_per_hour / 60
+
+    items_per_minute.floor
   end
 end
